2148c064982bf158da3c070b44a2d290d1f3506f
Hoorasadat <mirsaanei@gmail.com>
2019-01-26 23:19:33 -0700

    removing some unuseful parts-add sme more onfocus listener

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ if (document.getElementById('main')) {
 
     var imageArray = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
     var descriptionArray = ["Thiland", "Malaysia", "USA", "German"];
-    var imageUrl = ["https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.smartertravel.com%2Fuploads%2F2017%2F06%2Froad_trip_tips_hero-1400x500.jpg&imgrefurl=https%3A%2F%2Fwww.smartertravel.com%2F10-half-tips-road-trips%2F&docid=MPYyYTDIB8k0lM&tbnid=keZN4QQ7X7WGiM%3A&vet=10ahUKEwj--P7e6IngAhXKhlQKHV31Aw0QMwg-KAEwAQ..i&w=1400&h=500&bih=608&biw=1366&q=images%20for%20trips&ved=0ahUKEwj--P7e6IngAhXKhlQKHV31Aw0QMwg-KAEwAQ&iact=mrc&uact=8",
+    var imageUrlArray = ["https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.smartertravel.com%2Fuploads%2F2017%2F06%2Froad_trip_tips_hero-1400x500.jpg&imgrefurl=https%3A%2F%2Fwww.smartertravel.com%2F10-half-tips-road-trips%2F&docid=MPYyYTDIB8k0lM&tbnid=keZN4QQ7X7WGiM%3A&vet=10ahUKEwj--P7e6IngAhXKhlQKHV31Aw0QMwg-KAEwAQ..i&w=1400&h=500&bih=608&biw=1366&q=images%20for%20trips&ved=0ahUKEwj--P7e6IngAhXKhlQKHV31Aw0QMwg-KAEwAQ&iact=mrc&uact=8",
         "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimage.jimcdn.com%2Fapp%2Fcms%2Fimage%2Ftransf%2Fdimension%3D1280x10000%3Aformat%3Djpg%2Fpath%2Fsa6549607c78f5c11%2Fimage%2Fiebff385e8be4568d%2Fversion%2F1456062800%2Fheader-roadtrips-in-europe-amalfi-coast-copyright-marcelo-alex.jpg&imgrefurl=https%3A%2F%2Fwww.europeanbestdestinations.com%2Fbest-of-europe%2Fbest-roadtrips-in-europe%2F&docid=oFCHztfMgA1xXM&tbnid=Gnja_zIvW1Ko-M%3A&vet=10ahUKEwj--P7e6IngAhXKhlQKHV31Aw0QMwhvKCUwJQ..i&w=1280&h=457&bih=608&biw=1366&q=images%20for%20trips&ved=0ahUKEwj--P7e6IngAhXKhlQKHV31Aw0QMwhvKCUwJQ&iact=mrc&uact=8",
         "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.autoeurope.ca%2Fcae%2Fassets%2Fimage%2Fdrive-to-discover-italy.jpg&imgrefurl=https%3A%2F%2Fwww.autoeurope.ca%2Froad-trip-planner%2F&docid=Kr_wQBTK1YAatM&tbnid=J07CM4plptrIwM%3A&vet=10ahUKEwj--P7e6IngAhXKhlQKHV31Aw0QMwhzKCkwKQ..i&w=300&h=200&bih=608&biw=1366&q=images%20for%20trips&ved=0ahUKEwj--P7e6IngAhXKhlQKHV31Aw0QMwhzKCkwKQ&iact=mrc&uact=8#h=200&imgdii=fAh4WYh8zJFBvM:&vet=10ahUKEwj--P7e6IngAhXKhlQKHV31Aw0QMwhzKCkwKQ..i&w=300",
         "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.wildlandtrekking.com%2Fimg%2Fhomepicyellowstonebackpacking.jpg&imgrefurl=https%3A%2F%2Fwww.wildlandtrekking.com%2Ftrip-styles%2Fbackpacking-tours.html&docid=qds83odzvZau4M&tbnid=PEfFfBwNQVSlYM%3A&vet=10ahUKEwj--P7e6IngAhXKhlQKHV31Aw0QMwh0KCowKg..i&w=750&h=414&bih=608&biw=1366&q=images%20for%20trips&ved=0ahUKEwj--P7e6IngAhXKhlQKHV31Aw0QMwh0KCowKg&iact=mrc&uact=8"
@@ -31,7 +31,7 @@ if (document.getElementById('main')) {
 
         // adding the mouseclick function for the image to open a new window 
         newImg.addEventListener("click", function (event) {
-            var my_window = window.open(imageUrl[i]);
+            var my_window = window.open(imageUrlArray[i]);
 
             // adding a function to close the new window 
             function closeWin() {
@@ -108,7 +108,24 @@ if (document.getElementById('FormBody')) {
         dsLsFName.style.display = "none";
     });
 
+    var address = document.form.address;
+    var dsAddress = document.getElementById("descAddress");
+    address.addEventListener("focus", function (event) {
+        dsAddress.style.display = "block";
+    });
 
+    var postCode = document.form.postalCode;
+    var dsPosCod = document.getElementById("descPostalCode");
+    postCode.addEventListener("focus", function (event) {
+        dsAddress.style.display = "none";
+        dsPosCod.style.display = "block";
+    });
+
+    postCode.addEventListener("blur", function (event) {
+        dsPosCod.style.display = "none";
+    });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Error message
     var subButton = document.getElementById("SubmitButton");
     var erFsName = document.getElementById("errorFsName");
@@ -176,13 +193,13 @@ if (document.getElementById('FormBody')) {
     });
 
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Adding confirmation prompt before resetting
     var resetButton = document.getElementById("ResetButton");
 
     resetButton.addEventListener("click", function (event) {
         event.preventDefault();
-        var resetConfirm = confirm("Are you sure?");
+        var resetConfirm = confirm("Are you sure you want to reset?");
         if (resetConfirm) {
             document.form.reset();
         }
